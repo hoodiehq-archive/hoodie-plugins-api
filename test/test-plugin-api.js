78990483111b2d070b2f187e1e701408c44ec465
Caolan McMahon <caolan@caolanmcmahon.com>
2013-07-04 15:27:40 +0100

    add database.add, database.remove, database.findAll

--- test/test-plugin-api.js
+++ test/test-plugin-api.js
@@ -1,5 +1,7 @@
 var PluginAPI = require('../lib/index').PluginAPI,
-    utils = require('./lib/utils');
+    utils = require('./lib/utils'),
+    async = require('async'),
+    _ = require('underscore');
 
 
 var COUCH = {
@@ -25,8 +27,8 @@ exports.tearDown = function (callback) {
 };
 
 exports['request'] = function (test) {
-    var api = new PluginAPI(COUCH);
-    api.request('GET', '/', {}, function (err, data, res) {
+    var hoodie = new PluginAPI(COUCH);
+    hoodie.request('GET', '/', {}, function (err, data, res) {
         if (err) {
             return test.done(err);
         }
@@ -36,12 +38,36 @@ exports['request'] = function (test) {
 };
 
 exports['request as admin'] = function (test) {
-    var api = new PluginAPI(COUCH);
-    api.request('GET', '/_users/_all_docs', {}, function (err, data, res) {
+    var hoodie = new PluginAPI(COUCH);
+    hoodie.request('GET', '/_users/_all_docs', {}, function (err, data, res) {
         if (err) {
             return test.done(err);
         }
         test.equal(res.statusCode, 200);
+        test.done();
+    });
+};
+
+exports['database: add / findAll / remove'] = function (test) {
+    var hoodie = new PluginAPI(COUCH);
+    var database = hoodie.database;
+    async.series([
+        async.apply(_.bind(database.add, database), 'foo'),
+        async.apply(_.bind(database.add, database), 'bar'),
+        _.bind(database.findAll, database),
+        async.apply(_.bind(database.remove, database), 'foo'),
+        _.bind(database.findAll, database),
+        async.apply(_.bind(database.remove, database), 'bar'),
+        _.bind(database.findAll, database),
+    ],
+    function (err, results) {
+        var a = results[2][0],
+            b = results[4][0],
+            c = results[6][0];
+
+        test.same(a, ['bar', 'foo']);
+        test.same(b, ['bar']);
+        test.same(c, []);
         test.done();
     });
 };

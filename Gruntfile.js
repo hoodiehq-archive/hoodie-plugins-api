c23ce3ed34d62900920f1a29ed0452927bee80c8
Stephan Bönnemann <stephan@boennemann.me>
2014-08-02 17:14:28 +0200

    chore: migrate to grunt-release-hoodie v2 and new GH_TOKEN
    * * *
    
    This commit was sponsored by The Hoodie Firm.
    You can hire The Hoodie Firm:
    http://go.hood.ie/thehoodiefirm

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,4 @@
 module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-release-hoodie');
-
-  grunt.initConfig({
-    release: {
-      options: {
-        bump: {
-          files: ['package.json'],
-          commitFiles: ['package.json', 'CHANGELOG.md']
-        },
-        tasks: ['changelog']
-      }
-    }
-  });
-
   grunt.registerTask('ci', ['integration-test']);
 };
